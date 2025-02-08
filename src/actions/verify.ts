20f6e2d05d48bcd731619dfbdbaf751130818b61
gulshanpr <gulshankumarprasad04@gmail.com>
2025-02-08 08:58:46 +0530

    done from my side you need to add middleware. read comments carefully

--- src/actions/verify.ts
+++ src/actions/verify.ts
@@ -1,6 +1,22 @@
 "use server"
+// introduced for better type but dont know how to do it
+interface res {
+    confidence_score : number
+    matching_details : string
+    discripancies : string
+}
 
-export default function Verify(title: string, description: string){
+export default async function Verify(title: string, description: string, source_url: string){
     // verification logic with title and string.
+    const data = {
+        headline : title,
+        description,
+        source_url
+    }
+    const response : any =  await axios.post("Enter-Samyak's-Url-HEre", data);
+
+    const confidence_score : number = (response.confidence_score) * 100
+
+    // 90 is place holder value here , return the confidence_score here.
     return 90;
 }
--- src/app/submitNews/page.tsx
+++ src/app/submitNews/page.tsx
@@ -1,15 +1,17 @@
 "use client"
-
+// impore ui 
 import SubNews from "@/actions/submit"
 import Verify from "@/actions/verify"
 import { RainbowButton } from "@/components/ui/rainbow-button"
 import { useState } from "react"
 
-export default function PostNews() {
+export default async function PostNews() {
 
     const [title, setTitle] = useState<string>("")
     const [description, setDescrip] = useState<string>("")
     const [score, setScore] = useState<number>()
+    const [source, setSource] = useState<string>("")
+    const [id, setId] = useState<string>("")
     
     return <div>
         Post news
@@ -20,20 +22,27 @@ export default function PostNews() {
          <input type="text" placeholder="description" onChange={(e: any)=> {
             setDescrip(e.target.value)
         }} />
+        <input type="text" placeholder="enter a valid url source" onChange={(e: any)=> {
+            setSource(e.target.value)
+        }} />
         <RainbowButton 
-        onClick={()=> {
-            setScore(Verify(title, description));
+        onClick={async ()=> {
+            // returns score here
+            setScore( await Verify(title, description, source));
         }}
         >Verify news</RainbowButton>
 
         Score : {score}
 
-        <RainbowButton onClick={()=> {
+        <RainbowButton onClick={async ()=> {
             if (!score) {
                 alert("verify first")
-            } else if(score >= 80){
-                SubNews(title,description,score)
+            } else if(score >= 70){
+                // not required just added for confirmation for user
+               setId( await SubNews(title,description,score))
             }
         }}>submit news</RainbowButton>
+
+        submssion Id : {id}
     </div>
 }
